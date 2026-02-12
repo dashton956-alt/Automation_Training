c3b1d2a7e8e93e0cba633251465abcc25e093340
Dan Ashton <danielashton956@gmail.com>
2026-02-12 14:13:24 +0000

    Refactor Coffee Shop program to modularize order handling and add user-specific greeting logic; add initial project documentation and multiple new files for automation pipeline.

--- python/Learning/Coffe_shop.py
+++ python/Learning/Coffe_shop.py
@@ -4,22 +4,36 @@ print("Welcome to Robert's Coffee Shop!")
 
 name = input("What's your name?") 
 
-print("Hi " + name + ", what would you like to order?") 
+    
+def order():
+    print("Hi " + name + ", what would you like to order?") 
 
-menu = ["Espresso", "Latte", "Cappuccino", "Americano", "Mocha"] 
+    menu = ["Espresso", "Latte", "Cappuccino", "Americano", "Mocha"] 
 
-print("Menu:") 
+    print("Menu:") 
 
-for item in menu: print("- " + item) 
+    for item in menu: print("- " + item) 
 
-order = input("Please enter your order: ") 
+    order = input("Please enter your order: ") 
 
-cost = 5
+    cost = 5
 
-amount = input("How many would you like?")
+    amount = input("How many would you like?")
 
-total = cost * int(amount)
+    total = cost * int(amount)
 
-if order in menu: print("\nGreat choice, " + name + "! \nYour " + order + " will cost" + " £" + str(total) + "\nIt will be ready shortly" ) 
+    if order in menu: print("\nGreat choice, " + name + "! \nYour " + order + " will cost" + " £ "+ str(total) + "\nIt will be ready shortly" ) 
 
-else: print("Sorry, we don't have that on the menu. Please choose from the available options.")
+    else: print("Sorry, we don't have that on the menu. Please choose from the available options.")
+
+
+if name.lower() in ["ben", "pat"]:
+    evil_status = input("Are you still evil?\n")
+    if evil_status == "Yes" or evil_status == "yes":
+        print(" Your not welcome here! get out")
+        exit()
+    else:
+        order()
+        exit()
+else:
+    order()
